699a0a02634205d858eb5b20fdd8f4231aa1e572
saikrishna01301 <kittu.sai01301@gmail.com>
2024-08-23 17:29:46 -0400

    first commit

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const planetsRouter = require("./routes/planets.router");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+//d
+// Middleware to parse JSON bodies (must be above routes!)
+app.use(express.json());
+
+app.use(planetsRouter);
+
+module.exports = app;
---
+++ server/src/server.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const app = require("./app");
+const { loadPlanetsData } = require("./models/planets.models");
+
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+async function startServer() {
+  await loadPlanetsData();
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}...`);
+  });
+}
+
+startServer();
