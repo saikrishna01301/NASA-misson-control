137d47525a4ad6c41cfecea94da5452e10bbf937
saikrishna01301 <kittu.sai01301@gmail.com>
2024-10-02 11:05:34 -0400

    connected MongoDB through Mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,24 @@
 const http = require("http");
 const app = require("./app");
+const mongoose = require("mongoose");
 const { loadPlanetsData } = require("./models/planets.models");
 
 const PORT = process.env.PORT || 8000;
+const MONGO_URL =
+  "mongodb+srv://saikrishna01301:saikrishna01301@nasacluster.eaqna.mongodb.net/?retryWrites=true&w=majority&appName=NasaCluster";
 
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}...`);
